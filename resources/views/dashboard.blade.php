27ec754ffee5f38222502ab0d4c0faac0e94780c
aztanin <30766621+aztanin@users.noreply.github.com>
2020-10-08 13:15:03 +0600

    see whats going

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,7 +9,7 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
              Hi {{ Auth::user()->name }}
-             <p>hello<p/>
+             <p>hello Try new things</p>
             </div>
         </div>
     </div>
